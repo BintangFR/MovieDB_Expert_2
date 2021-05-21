6bec01734cf6add380ca6b9c136b55bbf2012d77
SovietChan <52289632+SovietChan@users.noreply.github.com>
2021-05-21 17:41:34 +0700

    Test deletion

--- app/src/test/java/com/example/moviedb/utils/TestExecutor.kt
+++
@@ -1,9 +0,0 @@
-package com.example.moviedb.utils
-
-import java.util.concurrent.Executor
-
-class TestExecutor : Executor {
-    override fun execute(command: Runnable?) {
-        command?.run()
-    }
-}
